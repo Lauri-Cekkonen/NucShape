ea4ed8f8b15fcd92f143e5226ab7dc99e3000da4
laaajoke <laaajoke@jyu.fi>
2026-06-30 16:35:45 +0300

    xytozmap (no min-max z yet)

--- src/C/asciimap.c
+++ src/C/asciimap.c
@@ -6,8 +6,8 @@
 enum zstreamtag {
   SOL     = 01, /* solution exists */
   NOSOL   = 02, /* solution does not exist */
-  NEWLINE = 04, /* newline when stream converted to
-                   2D array */
+  NEWLINE = 04, /* newline when stream converted
+                   to string and printed */
   END     = 010 /* end of stream */
 };
 
@@ -22,4 +22,27 @@ struct zstreamelem {
   } content;
 };
 
+/* Use function findz to map points (x, y)
+ * where x comes from array 
+ * [*xmin, *(xmin+1), ..., *xmax]
+ * and y comes from array
+ * [*ymin, *(ymin+1), ..., *ymax].
+ * Structs zstreamelem returned by findz
+ * are stored in array zstream (side effect). 
+ * Maximum amount of elements in zstream is
+ * znummax.*/
+void xytozmap(struct zstreamelem zstream[], int znummax,
+              double *xmin, double *xmax,
+              double *ymin, double *ymax,
+              struct zstreamelem (*findz)(double, double)) {
+  int i = 0;
+  double *xi, *yi;
 
+  for (xi = xmin; xi <= xmax; xi++) {
+    for (yi = ymin; yi <= ymin; yi++) {
+      if (i >= znummax)
+        return; /* array zstream full */
+      zstream[i++] = (*findz)(*xi, *yi);
+    }
+  }
+}
