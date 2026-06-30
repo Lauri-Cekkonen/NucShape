8a7d75bbf32fe498a1a20425b493fc3d3060c01a
laaajoke <laaajoke@jyu.fi>
2026-06-30 19:53:33 +0300

    added elemcmp

--- src/C/asciimap.c
+++ src/C/asciimap.c
@@ -15,12 +15,53 @@ enum zstreamtag {
 struct zstreamelem {
   zstreamtag tag;
   union {
-    double heigth; /*      for SOL */
+    double height; /*      for SOL */
     char   c;      /* ' '  for NOSOL,
                       '\n' for NEWLINE,
                       '\0' for END */
   } content;
 };
+
+typedef struct {
+  struct zstreamelem min;
+  struct zstreamelem max;
+} MinMaxPair;
+
+/* Return:
+ *  < 0 if elem1 < elem2
+ *  = 0 if elem1 = elem2
+ *  > 0 if elem1 > elem2
+ * If both elements have zstreamtag = SOL,
+ * the one with greater height is greater.
+ * If one element is SOL and the other one
+ * not, the SOL element is greater.
+ * Elements with other zstreamtag than SOL
+ * are equivalent. */
+int elemcmp(struct zstreamelem elem1,
+            struct zstreamelem elem2) {
+  switch (elem1.zstreamtag) {
+    case SOL:
+      switch (elem2.zstreamtag) {
+        case SOL:
+          if (elem1.content.height < elem2.content.height)
+            return -1;
+          if (elem1.content.height > elem2.content.height)
+            return 1;
+          return 0; 
+        default:
+          return 1;
+      }
+    default:
+      switch (elem2.zstreamtag) {
+        case SOL:
+          return -1;
+        default:
+          return 0;
+      }
+  }
+}
+
+
 
 /* Use function findz to map points (x, y)
  * where x comes from array 
@@ -31,12 +72,13 @@ struct zstreamelem {
  * are stored in array zstream (side effect). 
  * Maximum amount of elements in zstream is
  * znummax.*/
-void xytozmap(struct zstreamelem zstream[], int znummax,
+MinMaxPair xytozmap(struct zstreamelem zstream[], int znummax,
               double *xmin, double *xmax,
               double *ymin, double *ymax,
               struct zstreamelem (*findz)(double, double)) {
   int i = 0;
   double *xi, *yi;
+  MinMaxPair 
 
   for (xi = xmin; xi <= xmax; xi++) {
     for (yi = ymin; yi <= ymin; yi++) {
