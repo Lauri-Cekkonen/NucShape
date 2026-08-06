2fc7ed6ca81850a8a3cde996be8f2bd76b5c6370
laaajoke <laaajoke@jyu.fi>
2026-08-06 13:06:52 +0300

    polnottree.h: +applyunary (v1) and +applybinary

--- src/C/polnottree.h
+++ src/C/polnottree.h
@@ -8,11 +8,26 @@
 enum unaryoper {
   UNARYPLUS,     /* +x */
   UNARYMINUS,    /* -x */
-  SIN, COS, TAN, /* trigonometric
+  SIN, COS,      /* trigonometric
                     functions */
   EXP,           /* exponential
                     function */
   SQRT           /* square root */
+};
+
+/* Declaration of polnottnode
+ * for definitions of unarynode 
+ * and binarynode. */
+struct polnottnode; 
+
+/* The unary operation
+ * corresponding to 'flag'
+ * will be applied to the
+ * polnottnode object pointed
+ * to by 'next'. */  
+struct unarynode {
+  enum unaryoper flag;
+  struct polnottnode *next;
 };
 
 /* Operator flag corresponding 
@@ -20,11 +35,23 @@ enum unaryoper {
  * applied to the next left and
  * next right polnottnode 
  * objects. */
-enum binarynode {
+enum binaryoper {
   PLUS,  /* x + y */
   MINUS, /* x - y */
   PROD,  /* x * y */
   DIV    /* x / y */
+};
+
+/* The binary operation
+ * corresponding to 'flag'
+ * will be applied to the
+ * polnottnode objects pointed
+ * to by 'nextleft' and
+ * 'nextright'. */ 
+struct binarynode {
+  enum binaryoper flag;
+  struct polnottnode *nextleft;
+  struct polnottnode *nextright;
 };
 
 /* Type tag for polnottnode type.
@@ -50,27 +77,9 @@ struct polnottnode {
   enum nodetype tag;
   union {
     double scalar;
-    double (*funcptr)(Coordpoint);
-    struct {
-      /* The unary operation
-       * corresponding to 'flag'
-       * will be applied to the
-       * polnottnode object pointed
-       * to by 'next'. */
-      enum unaryoper flag;
-      struct polnottnode *next;
-    } unarynode;
-    struct {
-      /* The binary operation
-       * corresponding to 'flag'
-       * will be applied to the
-       * polnottnode objects pointed
-       * to by 'nextleft' and
-       * 'nextright'. */
-      enum binaryoper flag;
-      struct polnottnode *nextleft;
-      struct polnottnode *nextright;
-    } binarynode;
+    double (*func)(Coordpoint);
+    struct unarynode *unary;
+    struct binarynode *binary;
   } content;
 };
 
@@ -99,4 +108,81 @@ static enum applystatus apply(
     double *result)
 {
   switch (curr->tag) {
-    case 
+    case
+  }
+};
+
+static enum applystatus applyunary(
+    const struct unarynode *curr,
+    Coordpoint arg,
+    double *result)
+{
+  enum applystatus status;
+  double operand;
+
+  switch (curr->flag) {
+    case UNARYPLUS:
+      return apply(curr->next, arg, result);
+    case default:
+      status = apply(curr->next, arg, &operand);
+      if (status == APPLY_OK)
+        switch (curr->flag) {
+          case UNARYMINUS:
+            *result = -operand;
+            break;
+          case SIN:
+            *result = sin(operand);
+            break;
+          case COS:
+            *result = cos(operand);
+            break;
+          case EXP:
+            *result = exp(operand);
+            break;
+          case SQRT:
+            if (operand < 0)
+              return SQRT_OF_NEG;
+            *result = sqrt(operand);
+            break;
+          default:
+            return UNKNOWN_OPER;
+        };
+      return status;
+  }
+}
+
+static enum applystatus applybinary(
+    const struct binarynode *curr,
+    Coordpoint arg,
+    double *result)
+{
+  enum applystatus status1, status2;
+  double operand1, operand2;
+
+  status1 = apply(curr->nextleft, arg, &operand1);
+  if (status1 != APPLY_OK)
+    return status1; /* 'throw' left
+                       operand error */
+  status2 = apply(curr->nextright, arg, &operand2);
+  if (status2 == APPLY_OK)
+    switch (curr->flag) {
+      case PLUS:
+        *result = operand1 + operand2;
+        break;
+      case MINUS:
+        *result = operand1 - operand2;
+        break;
+      case PROD:
+        *result = operand1 * operand2;
+        break;
+      case DIV:
+        if (operand2 == 0.0)
+          return DIV_BY_ZERO;
+        *result = operand1 / operand2;
+        break;
+      default:
+        return UNKNOWN_OPER;
+    };
+  return status2;
+}
+
