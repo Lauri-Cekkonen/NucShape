449f70674ecb2f6b7749bd224c3cd50b42003af1
laaajoke <laaajoke@jyu.fi>
2026-08-28 23:11:28 +0300

    bruteforceroot catches apply error only if no solution is found

--- src/C/numsolvers.c
+++ src/C/numsolvers.c
@@ -10,9 +10,40 @@
  * than the given 'precision' ('brute 
  * force'):
  *   abs((*func)(x1)) < precision
- * where 'abs' is absolute value. 
- * 
- * NOTE: x1, x1min and x1max refer to 
+ * where 'abs' is absolute value.
+ *
+ * RETURN: In the following table,
+ * 'found?' means 'an approximative
+ * solution inside the precision
+ * was found?'.
+ *  
+ *  found?|apply_error?|return
+ *  ______|____________|__________
+ *  yes   |no          |SOLVED
+ *  yes   |yes         |SOLVED
+ *  no    |no          |UNSOLVABLE
+ *  no    |yes         |APPLY_ERROR
+ *  
+ * Note that if an approximative
+ * solution is found, any apply
+ * error found is ignored (the
+ * found apply error is not
+ * updated to 'applyerror'
+ * argument as a side effect).
+ * In general,  we ensure that 
+ * applystatus pointed to by 
+ * 'applyerror' has a valid
+ * (non-garbage) value as a side
+ * effect only  when 'bruteforceroot'
+ * returns 'APPLY_ERROR'. *
+ *
+ * If an approximative solution
+ * is found, the root is updated
+ * to 'foundroot' argument as a
+ * side effect.
+ *
+ *  
+ * NOTE: x1min and x1max refer to 
  * generic coordinate x1.
  *
  * NOTE: The function func(x1) is 
@@ -20,29 +51,23 @@
  * pointer *expr. Function application
  * is achieved with 'apply' function
  * from polnottree.c as
- *   appely(expr, p, result);
+ *   apply(expr, p, result);
  * where 'p' is a Coordpoint object
  * and 'result' points to a double
  * where the result of the application
  * is accumulated. p can even correspond 
  * to coordinates in multiple dimensions
- * (e.g. spherical coordinates) but the
- * algorithm of finding the root is 
+ * (e.g. 3D cartesian coordinates) but 
+ * the algorithm of finding the root is 
  * carried out along one-dimensional 
  * curve in the multi-dimensional space. 
  * This one-dimensional curve is in the
  * direction pointed to by
- * 'restofcoord->x1'. 
- *
- * NOTE: We only ensure that applystatus
- * pointed to by 'applyerror' has a valid
- * (non-garbage) value when 'bruteforceroot' 
- * returns 'APPLY_ERROR'. */
+ * 'restofcoord->x1'. */
 enum solverstatus bruteforceroot(
     const double *x1min, /* lower bound */
     const double *x1max, /* upper bound */
-    double *xi,          /* loop index and
-                            return value
+    double *foundroot,   /* return value
                             container */
     const struct polnottnode *expr, 
                          /* algorithm goal:
@@ -60,21 +85,32 @@ enum solverstatus bruteforceroot(
     double precision,
     enum applystatus *applyerror
                          /* make encountered
-                          * applystatus
-                          * signaling an
-                          * error
-                          * accessible */ )
+                          * apply error
+                          * accessible 
+                          * outside this
+                          * function */ )
 {
   double exprval;
-  enum applystatus status;
+  const double *x1; /* loop index */
+  enum applystatus status; /* status at each
+                              step in the
+                              loop */
+  enum applystatus error; /* container for
+                             apply error
+                             during the
+                             loop */
 
   if (restofcoord->x1 == NULL)
     return BAD_ARG; /* algorithm does not
                        know along which
                        coordinate to find
                        roots */
-  for (xi = (double *)x1min; xi < x1max; xi++) {
-    *(restofcoord->x1) = *xi; /* move forward
+  error = APPLY_OK; /* stays in this value
+                       unless apply error
+                       occurs during the
+                       loop */
+  for (x1 = x1min; x1 < x1max; x1++) {
+    *(restofcoord->x1) = *x1; /* move forward
                                  along the line
                                  to a new point
                                  for the algo-
@@ -85,17 +121,29 @@ enum solverstatus bruteforceroot(
       case APPLY_OK:
         /* note: no assignment to
          * applyerror */
-        if (fabs(exprval) < precision)
-          return SOLVED; /* solution found
-                            at xi */
+        if (fabs(exprval) < precision) {
+          /* precise enough solution found */
+          *foundroot = *x1;
+          return SOLVED;
+        }
         break;
       default:
-        *applyerror = status; /* "throw" apply 
-                                 error */
-        return APPLY_ERROR;
+        error = status; /* store found
+                           apply error */
+        break;
     }
   }
-  /* no solution found */
-  return UNSOLVABLE;
+  /* check for found apply error */
+  switch (error) {
+    case APPLY_OK:
+      /* neither a precise enough solution
+       * nor an apply error found */
+      return UNSOLVABLE;
+    default:
+      /* 'throw' the apply error that 
+       * occured last in the loop */
+      *applyerror = error;
+      return APPLY_ERROR;
+  }
 }
 
